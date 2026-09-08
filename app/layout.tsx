cefc5a9754fdc69b6bd19322aac5959880c7a07a
ideamosestudio <ideamosestudio@users.noreply.github.com>
2026-09-08 17:47:15 -0300

    Unificar CTAs desktop en 11px semibold y estilo naranja

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,7 @@ import "./home-module-spacing-trial.css";
 import "./desktop-layout-request-2026-08-31.css";
 import "./layout-request-2026-09-02.css";
 import "./mobile-layout-request-2026-09-02.css";
+import "./desktop-button-typography.css";
 
 const homeMetadata = createPageMetadata({
   title: "Diseño web y marketing digital | Ideamos",
